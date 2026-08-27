a6c8f3800a2fac17001d20fbc2c396228c23848d
Kshitij Pachpande <kshitijpachpande12@Gmail.com>
2026-08-27 16:48:51 +0530

    Ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { ConnectDB } from "./config/DB.js";
 import al_Routes from "./routes/al_Routes.js";
 import cors from "cors";
+import path from "path";
 
 dns.setServers(['8.8.8.8','1.1.1.1']);
 
@@ -11,13 +12,26 @@ dotenv.config()
 
 const PORT = process.env.PORT;
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 
 app.use(express.json());
 app.use("/api/Anime-list",al_Routes);
+
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../Frontend/dist","index.html"));
+    });
+}
 
 ConnectDB().then(()=>{
     const PORT = process.env.PORT || 5001;
