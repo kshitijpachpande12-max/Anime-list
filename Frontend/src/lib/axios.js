c44a9ce3c4f5fed19ac1d03215ba0670add7f366
Kshitij Pachpande <kshitijpachpande12@Gmail.com>
2026-08-27 17:41:36 +0530

    Ready to deploy

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const Base_url = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: Base_url,
 });
 
 export default api;
