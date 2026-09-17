7aaee0154c6cbb527cb69681be8172314f3e25b4
Kshitij Pachpande <kshitijpachpande12@Gmail.com>
2026-09-17 09:20:32 +0530

    optimized look for mobile

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
     <header className='bg-base-300 border-b border-base-content/10'>
     <div className='mx-auto max-w-6xl p-4'>
-        <div className='flex items-center justify-between'>
-            <Link to={"/watchlist"} className='btn btn-primary'>
+        <div className='flex items-center justify-between gap-2'>
+            <Link to={"/watchlist"} className='btn btn-primary btn-sm lg:btn-md'>
                 <Sparkle/>
                 <span className='lg:text-2xl text-xl'>Watchlist</span>
             </Link>
