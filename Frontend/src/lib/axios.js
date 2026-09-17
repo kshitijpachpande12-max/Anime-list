0a15fa92bcf348dd62edc4fe31e03da3880a56ce
Kshitij Pachpande <kshitijpachpande12@Gmail.com>
2026-09-17 09:01:58 +0530

    Connect frontend to deployed backend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-const Base_url = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const Base_url =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:5001/api"
+        : "https://anime-list-sigma.vercel.app/api";
 
 const api = axios.create({
     baseURL: Base_url,
