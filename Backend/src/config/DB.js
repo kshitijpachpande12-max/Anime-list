8190f7a4d5ec8c7a8f3b471864abaeb10c81d3d6
Kshitij Pachpande <kshitijpachpande12@Gmail.com>
2026-09-17 08:57:58 +0530

    Ready for vercel deploy

--- Backend/src/config/DB.js
+++ Backend/src/config/DB.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 export async function ConnectDB() {
     try {
-        mongoose.connect(process.env.DB_url);
+        await mongoose.connect(process.env.DB_url);
         console.log("Database connected successfully");   
     } catch (error) {
         console.error("Issue connecting to the database",error);
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,37 +4,18 @@ import dotenv from "dotenv";
 import { ConnectDB } from "./config/DB.js";
 import al_Routes from "./routes/al_Routes.js";
 import cors from "cors";
-import path from "path";
 
-dns.setServers(['8.8.8.8','1.1.1.1']);
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT;
 const app = express();
-const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production"){
-    app.use(cors({
-        origin: "http://localhost:5173",
-    }));
-}
-
+app.use(cors());
 app.use(express.json());
-app.use("/api/Anime-list",al_Routes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+app.use("/api/Anime-list", al_Routes);
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname,"../Frontend/dist","index.html"));
-    });
-}
+ConnectDB();
 
-ConnectDB().then(()=>{
-    const PORT = process.env.PORT || 5001;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-});
-
+export default app;
